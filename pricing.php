7bd0c339d124f40b78ec2a44002ea0d7e1302ace
abdulsalamkz <abdulsalamkhanzada@gmail.com>
2025-01-09 22:30:47 +0500

    create pricing page

---
+++ pricing.php
@@ -0,0 +1,209 @@
+<?php get_header()
+//Template Name:pricing
+?>
+<div class="container mx-auto px-4 py-12 lg:px-8">
+    <section>
+        <div class="grid grid-cols-1 gap-5 lg:grid-cols-2 mb-24 items-center ">
+            <div>
+                <h1 class="text-4xl font-bold text-gray-15 mb-4">
+                    Our Pricings
+                </h1>
+            </div>
+            <div>
+                <p class="text-gray-35">
+                    Welcome to SkillBridge's Pricing Plan page, where we offer two comprehensive options to cater to your needs: Free and Pro. We believe in providing flexible and affordable pricing options for our services. Whether you're an individual looking to enhance your skills or a business seeking professional development solutions, we have a plan that suits you. Explore our pricing options below and choose the one that best fits your requirements.
+                </p>
+            </div>
+        </div>
+        <div class="flex justify-center">
+            <span class="flex items-center justify-center bg-absolute-white h-16 w-48 rounded-lg">
+                <button class="py-3 text-absolute-white bg-orange-50 px-5 rounded-lg">
+                    Monthly
+                </button>
+                <button class="py-3 px-5 text-gray-30">
+                    Yearly
+                </button>
+            </span>
+        </div>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-8 lg:mt-10 mb-24 mt-4 lg:p-20 p-6 rounded-xl bg-absolute-white shadow-md">
+            <div class="rounded-lg bg-white-99 p-3 border border-white-95">
+                <div class="flex justify-center items-center p-2 rounded border border-orange-90 bg-orange-97">
+                    <h6 class="text-xl text-gray-15 font-medium">Free Plan</h6>
+                </div>
+                <div class="flex justify-center">
+                    <h3 class="text-3xl p-5">
+                        <span class="text-7xl text-gray-15 font-semibold">$0</span>
+                        <span class="text-xl text-gray-30 font-medium">/month</span>
+                    </h3>
+                </div>
+                <div class="flex-col lg:p-8 p-4 pb-4 items-center gap-6 border bg-absolute-white border-white-95 rounded-t-lg">
+                    <h3 class="text-xl font-medium text-center mb-5">Available Features</h3>
+                    <ul>
+                        <li class="flex items-center p-3 gap-2 rounded-md border border-white-95 mb-4">
+                            <span class="h-8 w-8 flex items-center justify-center bg-orange-95 rounded-md">
+                                <i class="fas fa-check"></i>
+                            </span>
+                            <p class="text-grey-30 md:text-lg text-sm">Access to selected free courses.</p>
+                        </li>
+                        <li class="flex items-center p-3 gap-2 rounded-md border border-white-95 mb-4">
+                            <span class="h-8 w-8 flex items-center justify-center bg-orange-95 rounded-md">
+                                <i class="fas fa-check"></i>
+                            </span>
+                            <p class="text-grey-30 md:text-lg text-sm">Limited course materials and resources.</p>
+                        </li>
+                        <li class="flex items-center p-3 gap-2 rounded-md border border-white-95 mb-4">
+                            <span class="h-8 w-8 flex items-center justify-center bg-orange-95 rounded-md">
+                                <i class="fas fa-check"></i>
+                            </span>
+                            <p class="text-grey-30 md:text-lg text-sm">Basic community support.</p>
+                        </li>
+                        <li class="flex items-center p-3 gap-2 rounded-md border border-white-95 mb-4">
+                            <span class="h-8 w-8 flex items-center justify-center bg-orange-95 rounded-md">
+                                <i class="fas fa-check"></i>
+                            </span>
+                            <p class="text-grey-30 md:text-lg text-sm">No certification upon completion.</p>
+                        </li>
+                        <li class="flex items-center p-3 gap-2 rounded-md border border-white-95 mb-4">
+                            <span class="h-8 w-8 flex items-center justify-center bg-orange-95 rounded-md">
+                                <i class="fas fa-check"></i>
+                            </span>
+                            <p class="text-grey-30 md:text-lg text-sm">Ad-supported platform.</p>
+                        </li>
+                        <li class="flex items-center p-3 gap-2 rounded-md border border-white-95 mb-4">
+                            <span class="h-8 w-8 flex items-center justify-center border border-white-95 rounded-md">
+                                <i class="fa-solid fa-xmark"></i>
+                            </span>
+                            <p class="text-grey-30 md:text-lg text-sm">Access to exclusive Pro Plan community forums.</p>
+                        </li>
+                        <li class="flex items-center p-3 gap-2 rounded-md border border-white-95 mb-4">
+                            <span class="h-8 w-8 flex items-center justify-center border border-white-95 rounded-md">
+                                <i class="fa-solid fa-xmark"></i>
+                            </span>
+                            <p class="text-grey-30 md:text-lg text-sm">Early access to new courses and updates.</p>
+                        </li>
+
+                    </ul>
+                </div>
+                <button class="p-3.5 text-absolute-white bg-orange-50 rounded-b-lg w-full">Get it Now</button>
+            </div>
+            <div class="rounded-lg bg-white-99 p-3 border border-white-95">
+                <div class="flex justify-center items-center p-2 rounded border border-orange-90 bg-orange-97">
+                    <h6 class="text-xl text-gray-15 font-medium">Pro Plan</h6>
+                </div>
+                <div class="flex justify-center">
+                    <h3 class="text-3xl p-5">
+                        <span class="text-7xl text-gray-15 font-semibold">$79</span>
+                        <span class="text-xl text-gray-30 font-medium">/month</span>
+                    </h3>
+                </div>
+                <div class="flex-col lg:p-8 p-4 pb-4 items-center gap-6 border bg-absolute-white border-white-95 rounded-t-lg">
+                    <h3 class="text-xl font-medium text-center mb-5">Available Features</h3>
+                    <ul>
+                        <li class="flex items-center p-3 gap-2 rounded-md border border-white-95 mb-4">
+                            <span class="h-8 w-8 flex items-center justify-center bg-orange-95 rounded-md">
+                                <i class="fas fa-check"></i>
+                            </span>
+                            <p class="text-grey-30 md:text-lg text-sm">Unlimited access to all courses.</p>
+                        </li>
+                        <li class="flex items-center p-3 gap-2 rounded-md border border-white-95 mb-4">
+                            <span class="h-8 w-8 flex items-center justify-center bg-orange-95 rounded-md">
+                                <i class="fas fa-check"></i>
+                            </span>
+                            <p class="text-grey-30 md:text-lg text-sm">Unlimited course materials and resources.</p>
+                        </li>
+                        <li class="flex items-center p-3 gap-2 rounded-md border border-white-95 mb-4">
+                            <span class="h-8 w-8 flex items-center justify-center bg-orange-95 rounded-md">
+                                <i class="fas fa-check"></i>
+                            </span>
+                            <p class="text-grey-30 md:text-lg text-sm">Priority support from instructors.</p>
+                        </li>
+                        <li class="flex items-center p-3 gap-2 rounded-md border border-white-95 mb-4">
+                            <span class="h-8 w-8 flex items-center justify-center bg-orange-95 rounded-md">
+                                <i class="fas fa-check"></i>
+                            </span>
+                            <p class="text-grey-30 md:text-lg text-sm">Course completion certificates.</p>
+                        </li>
+                        <li class="flex items-center p-3 gap-2 rounded-md border border-white-95 mb-4">
+                            <span class="h-8 w-8 flex items-center justify-center bg-orange-95 rounded-md">
+                                <i class="fas fa-check"></i>
+                            </span>
+                            <p class="text-grey-30 md:text-lg text-sm">Ad-free experience.</p>
+                        </li>
+                        <li class="flex items-center p-3 gap-2 rounded-md border border-white-95 mb-4">
+                            <span class="h-8 w-8 flex items-center justify-center bg-orange-95 rounded-md">
+                                <i class="fas fa-check"></i>
+                            </span>
+                            <p class="text-grey-30 md:text-lg text-sm">Access to exclusive Pro Plan community forums.</p>
+                        </li>
+                        <li class="flex items-center p-3 gap-2 rounded-md border border-white-95 mb-4">
+                            <span class="h-8 w-8 flex items-center justify-center bg-orange-95 rounded-md">
+                                <i class="fas fa-check"></i>
+                            </span>
+                            <p class="text-grey-30 md:text-lg text-sm">Early access to new courses and updates.</p>
+                        </li>
+
+                    </ul>
+                </div>
+                <button class="p-3.5 text-absolute-white bg-orange-50 rounded-b-lg w-full">Get it Now</button>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class=" flex flex-col lg:flex-row lg:p-10 p-8 rounded-xl bg-absolute-white shadow-md">
+            <div class="flex flex-col w-full lg:w-5/12 lg:pr-10">
+                <div>
+                    <h3 class="text-gray-15 text-4xl font-semibold leading-[1.3] mb-4">
+                        Frequently<br>Asked Questions
+                    </h3>
+                    <p class="text-absolute-black text-base leading-6">
+                        Still have any questions? Contact our team via support@skillbridge.com
+                    </p>
+                </div>
+                <div class="mt-6">
+                    <button type="button"
+                        class="flex p-4 justify-center items-center gap-2 rounded-md border border-white-95 bg-white text-absolute-black hover:bg-white-97 shadow-sm text-sm font-medium">
+                        See All FAQ’s
+                    </button>
+                </div>
+            </div>
+            <div class="flex flex-col w-full lg:w-7/12 mt-10 lg:mt-0">
+                <div class="rounded-lg bg-white shadow-sm p-6">
+                    <ul class="list-none">
+                        <li class="flex justify-between items-center pb-5 border-b border-white-90 text-gray-15 text-lg font-medium">
+                            Can I enroll in multiple courses at once?
+                            <a href="#" class="bg-orange-95 rounded-lg h-14 w-14 flex justify-center items-center "><i class="fas fa-times text-gray-15"></i></a>
+                        </li>
+                        <li class="mt-5 mb-6 text-gray-30 text-base leading-6">
+                            Absolutely! You can enroll in multiple courses simultaneously and access them at your convenience.
+                        </li>
+                        <li class="flex justify-between items-center p-4 rounded-md border border-white-95 bg-white-95 hover:bg-white-97 cursor-pointer">
+                            Enrollment Process for Different Courses
+                            <a href="#" class="bg-absolute-white px-5 py-4 rounded-full"><i class="fas fa-arrow-right text-gray-15"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="mt-6">
+                    <ul class="p-0 m-0 list-none space-y-4">
+                        <li class="flex items-center justify-between p-5 rounded-lg border border-white-95 bg-white-99 hover:bg-white-97 cursor-pointer">
+                            <span class="text-gray-15">What kind of support can I expect from instructors?</span>
+                            <a href="#" class="bg-orange-95 rounded-lg h-14 w-14 flex justify-center items-center "><i class="fas fa-plus text-gray-15"></i></a>
+                        </li>
+                        <li class="flex items-center justify-between p-5 rounded-lg border border-white-95 bg-white-99 hover:bg-white-97 cursor-pointer">
+                            <span class="text-gray-15">Are the courses self-paced or do they have specific start and end dates?</span>
+                            <a href="#" class="bg-orange-95 rounded-lg h-14 w-14 flex justify-center items-center "><i class="fas fa-plus text-gray-15"></i></a>
+                        </li>
+                        <li class="flex items-center justify-between p-5 rounded-lg border border-white-95 bg-white-99 hover:bg-white-97 cursor-pointer">
+                            <span class="text-gray-15">Are there any prerequisites for the courses?</span>
+                            <a href="#" class="bg-orange-95 rounded-lg h-14 w-14 flex justify-center items-center "><i class="fas fa-plus text-gray-15"></i></a>
+                        </li>
+                        <li class="flex items-center justify-between p-5 rounded-lg border border-white-95 bg-white-99 hover:bg-white-97 cursor-pointer">
+                            <span class="text-gray-15">Can I download the course materials for offline access?</span>
+                            <a href="#" class="bg-orange-95 rounded-lg h-14 w-14 flex justify-center items-center"><i class="fas fa-plus text-gray-15"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<?php get_footer() ?>
